784b76dbd6724b234a536967891ce0c698da3d5a
NasimbayevFarhod <105714502+NasimbayevFarhod@users.noreply.github.com>
2022-09-20 23:50:31 +0500

    .

--- js/main.js
+++ js/main.js
@@ -37,12 +37,27 @@ var sayohatEUR = 120
 var JamiHarajatlarSomda = (biletUSD + mehmonhonaUSD) * dollorSomda + sayohatEUR*yevroSomda
 
 console.log(JamiHarajatlarSomda);
+console.log(pul);
+console.log(typeof(pul));
 
 let span = document.querySelector(".span");
+
 
   if (JamiHarajatlarSomda <= pul){
     span.innerHTML =  ("Oq yo'l dostim")
   }
+
+  if (pul == null) 
+  {
+    span.innerHTML =  ("Siz malumot kiritmadingiz saytni yangilab mablag'ingizni tog'ri kiriting")
+  }
+
+  // if (typeof(pul) == String) 
+  // {
+  //   span.innerHTML =  ("Oq yo'l dostim")
+  //   console.log("asdasdasdasda");
+  // }
+
   else
   {
     span.innerHTML =  ("Afsuski Mablag'ingiz yitarli emas, Yana ozgina sabir qilishingizga tog'ri kelar ekan")
